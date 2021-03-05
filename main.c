a4058dcc510f15705e1dc025d39f7b4dcfc5d745
jreifel <80080688+jreifel@users.noreply.github.com>
2021-03-05 14:12:00 -0500

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,11 @@ double sidelength(double x1, double x2, double y1, double y2);
 double getPerimeter(double side1, double side2, double side3);
 double getCenter(double p1, double p2, double p3, double side1, double side2, double side3, double perimeter);
 double getAngle(double side1, double side2, double sideAngle);
+double getAngleRemainder(double angle);
+double getCenterToSide(double toCenter, double angle);
+double getQuadSide(double toCenter, double angle);
+double getAreaQuad(double s, double side1, double side2, double side3, double side4);
+void display(double AngleA, double AngleB, double AngleC, double AngleAMinutes, double AngleBMinutes, double AngleCMinutes, double AngleASeconds, double AngleBSeconds, double AngleCSeconds, double areaA, double areaB, double areaC);
 
 int main()
 {
@@ -31,6 +36,15 @@ int main()
   double angleASec;
   double angleBSec;
   double angleCSec;
+  double side1Center; //The distance from 1 to the Center
+  double side2Center; //The distance from 2 to the Center
+  double side2Center; //The distance from 2 to the Center
+  double quad1Area; //The area of the quadrilateral connecting to point 1
+  double quad2Area; //The area of the quadrilateral connecting to point 2
+  double quad3Area; //The area of the quadrilateral connecting to point 3
+  double quad1Side; //The side of quad 1 on the triangle
+  double quad2Side; //The side of quad 2 on the triangle
+  double quad3Side; //The side of quad 3 on the triangle
 
   //inputs
   xcoord1 = getCoordinate("X", 1);
@@ -63,9 +77,21 @@ int main()
   angleCSec = getAngleRemainder(angleCMin);
 
   //calculating the lines perpendicular to sides from center
-
+  side1Center = sideLength(xcoord1, centerPointx, ycoord1, centerPointy);
+  side2Center = sideLength(xcoord2, centerPointx, ycoord2, centerPointy);
+  side3Center = sideLength(xcoord3, centerPointx, ycoord3, centerPointy);
+  side1CenterAngle = angleA / 2;
+  side2CenterAngle = angleB / 2;
+  side3CenterAngle = angleC / 2;
+  radius = getCenterToSide(side1Center, side1CenterAngle);
+  
   //calculating the areas of the quadrilaterals
-
+  quad1Area = getAreaQuad(radius, radius, quad1Side, quad1Side);
+  quad2Area = getAreaQuad(radius, radius, quad2Side, quad2Side);
+  quad3Area = getAreaQuad(radius, radius, quad3Side, quad3Side);
+  
+  //outputting
+  
   return 0;
 }
 
@@ -121,6 +147,49 @@ double getAngleRemainder(double angle)
   return remainder;
 }
 
+double getCenterToSide(double toCenter, double angle)
+{
+  double centerToSide;
+  angle *= M_PI / 180;
+  centerToSide = toCenter * sin(angle);
+  return centerToSide;
+}
 
+double getQuadSide(double toCenter, double angle)
+{
+  double quadSide;
+  angle *= M_PI / 180;
+  quadSide = toCenter * cos(angle);
+  return quadSide;
+}
 
+double getAreaQuad(double s, double side1, double side2, double side3, double side4)
+{
+  double area;
+  area = (s - side1) * (s - side2) * (s - side3) * (s - side4);
+  area = sqrt(area);
+  return area;
+}
+
+void display(double AngleA, double AngleB, double AngleC, double AngleAMinutes, double AngleBMinutes, double AngleCMinutes, double AngleASeconds, double AngleBSeconds, double AngleCSeconds, double areaA, double areaB, double areaC)
+{
+  double PrintAngleA;
+  double PrintAngleB;
+  double PrintAngleC;
+  double PrintMinutesA;
+  double PrintMinutesB;
+  double PrintMinutesC;
+  
+  PrintAngleA = floor(AngleA);
+  PrintAngleB = floor(AngleB);
+  PrintAngleC = floor(AngleC);
+  PrintMinutesA = floor(AngleAMinutes);
+  PrintMinutesB = floor(AngleBMinutes);
+  PrintMinutesC = floor(AngleCMinutes);
+  
+  printf("Internal triangle angle %.0lf° %.0lf' %.2lf\", %.0lf° %.0lf' %.2lf\", %.0lf° %.0lf' %.2lf\"", AngleA, PrintMinutesA, AngleASeconds, AngleB, PrintMinutesB, AngleBSeconds, AngleC, PrintMinutesC, AngleCSeconds);
+  printf("Area of quadrilaterals: %.2lf, %.2lf, %.2lf", areaA, areaB, areaC);
+  
+  return (0);
+}
 
