4211d736f46085eef3db1c106e23762981c7a421
jreifel <80080688+jreifel@users.noreply.github.com>
2021-03-04 23:30:00 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,61 +5,76 @@ double getCoordinate(char plane, int num);
 double sidelength(double x1, double x2, double y1, double y2);
 double getPerimeter(double side1, double side2, double side3);
 double getCenter(double p1, double p2, double p3, double side1, double side2, double side3, double perimeter);
+double getAngle(double side1, double side2, double sideAngle);
 
 int main()
 {
-	//variable declarations
-	double xcoord1;
-	double xcoord2;
-	double xcoord3;
-	double ycoord1;
-	double ycoord2;
-	double ycoord3;
-	double sideLengthA; //The length from points 1 to 2
-	double sideLengthB; //The length from points 2 to 3
-	double sideLengthC; //The length from points 3 to 1
-	double perimeter;
-	double centerPointx;
-	double centerPointy;
-	double angleA; //The angle across from side A
-	double angleB; //The angle across from side B 
-	double angleC; //The angle across from side C
+  //variable declarations
+  double xcoord1;
+  double xcoord2; 
+  double xcoord3;
+  double ycoord1;
+  double ycoord2;
+  double ycoord3;
+  double sideLengthA; //The length from points 1 to 2
+  double sideLengthB; //The length from points 2 to 3
+  double sideLengthC; //The length from points 3 to 1
+  double perimeter;
+  double centerPointx;
+  double centerPointy;
+  double angleA; //The angle across from side A
+  double angleB; //The angle across from side B 
+  double angleC; //The angle across from side C
+  double angleAMin;
+  double angleBMin;
+  double angleCMin;
+  double angleASec;
+  double angleBSec;
+  double angleCSec;
 
   //inputs
   xcoord1 = getCoordinate("X", 1);
-	ycoord1 = getCoordinate("Y", 1);
-	xcoord2 = getCoordinate("X", 2);
-	ycoord2 = getCoordinate("Y", 2);
-	xcoord3 = getCoordinate("X", 3);
-	ycoord3 = getCoordinate("Y", 3);
+  ycoord1 = getCoordinate("Y", 1);
+  xcoord2 = getCoordinate("X", 2);
+  ycoord2 = getCoordinate("Y", 2);
+  xcoord3 = getCoordinate("X", 3);
+  ycoord3 = getCoordinate("Y", 3);
 
-	//calculations:
-	//calculating sideLengths
+  //calculations: 
+  //calculating sideLengths
   sideLengthA = sideLength(xcoord1, xcoord2, ycoord1, ycoord2);
-	sideLengthB = sideLength(xcoord2, xcoord3, ycoord2, ycoord3);
-	sideLengthC = sideLength(xcoord3, xcoord1, ycoord3, ycoord1);
+  sideLengthB = sideLength(xcoord2, xcoord3, ycoord2, ycoord3);
+  sideLengthC = sideLength(xcoord3, xcoord1, ycoord3, ycoord1);
 
-	//calculating center of the triangle
+  //calculating center of the triangle
   perimeter = getPerimeter(sideLengthA, sideLengthB, sideLengthC);
   centerPointx = getCenter(xcoord1, xcoord2, xcoord3, sideLengthA, sideLengthB, sideLengthC, perimeter);
-	centerPointy = getCenter(ycoord1, ycoord2, ycoord3, sideLengthA, sideLengthB, sideLengthC, perimeter);
+  centerPointy = getCenter(ycoord1, ycoord2, ycoord3, sideLengthA, sideLengthB, sideLengthC, perimeter);
 
-	//calculating angles
-  
+  //calculating angles
+  angleA = getAngle(sideLengthB, sideLengthC, sideLengthA);
+  angleB = getAngle(sideLengthA, sideLengthB, sideLengthB);
+  angleC = getAngle(sideLengthA, sideLengthB, sideLengthC);
+  angleAMin = getAngleRemainder(angleA);
+  angleBMin = getAngleRemainder(angleB);
+  angleCMin = getAngleRemainder(angleC);
+  angleASec = getAngleRemainder(angleAMin);
+  angleBSec = getAngleRemainder(angleBMin);
+  angleCSec = getAngleRemainder(angleCMin);
 
-	//calculating the lines perpendicular to sides from center
+  //calculating the lines perpendicular to sides from center
 
-	//calculating the areas of the quadrilaterals
+  //calculating the areas of the quadrilaterals
 
-	return 0;
+  return 0;
 }
 
 double getCoordinate(char plane, int num)
 {
   double point;
-	printf("Enter %c coordinate #%i ->", plane, num); 
-	scanf("%lf", &point);
-	return point;
+  printf("Enter %c coordinate #%i ->", plane, num); 
+  scanf("%lf", &point);
+  return point;
 }
 
 double sidelength(double x1, double x2, double y1, double y2)
@@ -76,24 +91,36 @@ double sidelength(double x1, double x2, double y1, double y2)
 double getPerimeter(double side1, double side2, double side3)
 {
   double perimeter;
-	perimeter = side1 + side2 + side3;
-	return perimeter;
+  perimeter = side1 + side2 + side3;
+  return perimeter;
 }
 
 double getCenter(double p1, double p2, double p3, double side1, double side2, double side3, double perimeter)
 {
-	double pCenter;
-	pCenter = p1 * side1 + p2 * side2 + p3 * side3;
-	pCenter /= perimeter;
-	return pCenter;
+  double pCenter;
+  pCenter = p1 * side1 + p2 * side2 + p3 * side3;
+  pCenter /= perimeter;
+  return pCenter;
 }
 
 double getAngle(double side1, double side2, double sideAngle)
 {
-	double angle;
-	angle = pow(side1, 2) + pow(side2, 2) - pow(sideAngle, 2);
-	angle /= (2 * side1 * side2);
-	angle = acos(angle);
+  double angle;
+  angle = pow(side1, 2) + pow(side2, 2) - pow(sideAngle, 2);
+  angle /= (2 * side1 * side2);
+  angle = acos(angle);
   angle *= (180 / M_PI);
-	return angle;
+  return angle;
 }
+
+double getAngleRemainder(double angle)
+{
+  double remainder;
+  remainder = a - floor(a);
+  remainder *= 60;
+  return remainder;
+}
+
+
+
+
